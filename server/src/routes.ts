5f51c61bad71fda08d50132433c81f47c1023860
Luiz Rech <luizadre@hotmail.com>
2020-06-05 18:17:51 -0300

    create and show routes, some configs in db, script

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 
+import PointsController from './controllers/pointsController';
+import ItemsController from './controllers/itemsController';
+
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({message: "Servidor UP!"});
-});
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
