459fe418024177fb89ef299af54cc119e269e43f
Luiz Rech <luizadre@hotmail.com>
2020-06-07 16:24:37 -0300

    fileUpload

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -7,6 +7,7 @@ import axios from 'axios';
 
 import logo from '../../assets/logo.svg';
 import api from '../../services/api';
+import Dropzone from '../../components/dropzone/index';
 
 import './styles.css';
 
@@ -38,6 +39,8 @@ const CreatePoint = () => {
 		const [selectedCity, setSelectedCity] = useState<string>('0');
 
 		const [selectedItems, setSelectedItems] = useState<number[]>([]);
+
+		const [selectedFile, setSelectedFile] = useState<File>();
 
 		const [formData, setFormData] = useState({
 			name: '',
@@ -124,23 +127,29 @@ const CreatePoint = () => {
 
 		async function handleSubmitForm(event: FormEvent){
 			event.preventDefault();
+
+
 			const {name, email, whatsapp} = formData;
 			const uf = selectedUf;
 			const city = selectedCity;
 			const [latitude, longitude] = selectedPositionMap;
 			const items = selectedItems;
 
-			const data = {
-				name,
-				email,
-				whatsapp,
-				uf,
-				city,
-				latitude,
-				longitude,
-				items
-			};
+			const data = new FormData();
 
+			data.append('name', name);
+			data.append('email', email);
+			data.append('whatsapp', whatsapp);
+			data.append('uf', uf);
+			data.append('city', city);
+			data.append('latitude', String(latitude));
+			data.append('longitude', String(longitude));
+			data.append('items', items.join(','));
+
+			if(selectedFile){
+				data.append('image', selectedFile);
+			}
+	
 			await api.post('points', data);
 
 			alert("Cadastrou")
@@ -159,9 +168,11 @@ const CreatePoint = () => {
 					</Link>
 				</header>
 
+
 				<form onSubmit={handleSubmitForm}>
 					<h1>Cadastro do <br /> novo ponto de coleta</h1>
 
+					<Dropzone onFileUploaded={setSelectedFile}/>
 					<fieldset>
 						<legend>
 							<h2>Dados</h2>
